558988bca58d9c45c4a017c50cc4df9bda098682
AleTodaroCbr650r <115482662+AleTodaroCbr650r@users.noreply.github.com>
2022-11-24 18:45:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+   int n=0,primo=1,secondo=1,terzo=0;
+
+    cin >> n;
+
+    if(n>=2) {
+        cout << primo << endl << secondo << endl;
+
+        for (int i = 2; i < n; i++) {
+
+            terzo = primo + secondo;
+            primo = secondo;
+            secondo = terzo;
+            cout << terzo << endl;
+
+        }
+
+    }else{
+        cout << "errore" << endl;
+    }
    return 0;
 }
